d04acb4fd3d8583b61a1f3e56e634ee6b8d5a0dd
Sergio Peña <sergiop79@gmail.com>
2025-06-21 19:34:47 +0200

    Start repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,27 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
 export function cleanInput(input: string): string[] {
   return input.split(' ').filter((word) => word.length);
+}
+
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: 'Pokedex > ',
+  });
+
+  rl.prompt();
+
+  rl.on('line', (input) => {
+    const command = cleanInput(input);
+
+    if (command.length === 0) {
+      rl.prompt();
+    } else {
+      console.log(`Your command was: ${command[0]}`);
+      rl.prompt();
+    }
+  });
 }
