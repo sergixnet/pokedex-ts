48d209de338267981a121318b4459b024082c223
Sergio Peña <sergiop79@gmail.com>
2025-06-21 19:13:56 +0200

    Initial commit

---
+++ src/repl.test.ts
@@ -0,0 +1,29 @@
+import { cleanInput } from './repl';
+import { describe, expect, test } from 'vitest';
+
+describe.each([
+  {
+    input: '  hello  world  ',
+    expected: ['hello', 'world'],
+  },
+  {
+    input: 'once upon a time...',
+    expected: ['once', 'upon', 'a', 'time...'],
+  },
+  {
+    input: '   uno dos y        tres    ',
+    expected: ['uno', 'dos', 'y', 'tres'],
+  },
+])('cleanInput($input)', ({ input, expected }) => {
+  test(`Expected: ${expected}`, () => {
+    // TODO: call cleanInput with the input here
+    const actual = cleanInput(input);
+    // The `expect` and `toHaveLength` functions are from vitest
+    // they will fail the test if the condition is not met
+    expect(actual).toHaveLength(expected.length);
+    for (const i in expected) {
+      // likewise, the `toBe` function will fail the test if the values are not equal
+      expect(actual[i]).toBe(expected[i]);
+    }
+  });
+});
